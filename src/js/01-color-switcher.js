3b11e4b9c808425db0759de5fe3b2ffe1dfed5fd
Tetiana <tatyanadubasova1976@gmail.com>
2023-02-01 21:24:45 +0200

    project 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+const { btnStart, btnStop, body } = refs;
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStoptClick);
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+function onBtnStoptClick() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
